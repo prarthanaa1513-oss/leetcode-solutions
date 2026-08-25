8ee35cfbd8c8b79c491538220329cdda15fee2e3
prarthanaa1513-oss <prarthanaa1513@gmail.com>
2026-08-25 19:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (50.57%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -0,0 +1,11 @@
+class Solution(object):
+
+    def missingMultiple(self, nums, k):
+        nums_set = set(nums)
+
+        multiple = k
+
+        while multiple in nums_set:
+            multiple += k
+
+        return multiple
